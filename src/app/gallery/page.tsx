5c6d2ac3bff9eba6b3c42816de8e583cb4790b36
Jaweria321 <jaweria.ehsan123@gmail.com>
2024-07-29 00:42:25 +0500

    working on gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,37 @@
+import { CloudinaryImage } from "./cloudinary-image";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
+
+type SearchResult = {
+  public_id: string;
+};
+export default async function GalleryPage() {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("public_id", "desc")
+    .max_results(10)
+    .execute()) as { resources: SearchResult[] };
+  console.log(results);
+  return (
+    <section>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
+
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              width="300"
+              height="400"
+              alt="an image of something"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
